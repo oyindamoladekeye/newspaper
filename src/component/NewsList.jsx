eeed45bc6819d5edbad0a84ea161cbcc2f9c7a4f
Adekeye Oyindamola <oyindamoladekeye@gmail.com>
2023-09-19 03:04:24 +0100

    updated api

--- src/component/NewsList.jsx
+++ src/component/NewsList.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from "axios"
-import {useState, useE} from "react"
+import {useState, useEffect} from "react"
 import { useParams }from "react-router-dom"
 
 export default function () {
@@ -10,7 +10,7 @@ const newsItems = news.slice(0,80)
 
 useEffect(() => {
   const loadNews= async() =>{
-  const response = await axios.get("https://newsapi.org/v2/top-headlines?country=ca&category=${category}&apiKey=deb3b99327ec422cb41ec796dbc447c0")
+  const response = await axios.get("https://gnews.io/api/v4/top-headlines?category=${category}&apikey=78d6be771e1e9792df2333dcfba57b5c")
   setNews(response.data.articles)
 };
 loadNews();
@@ -23,7 +23,7 @@ console.log("news",news)
       newsItems.map((items, index) =>{
         return(
             <div className='flex flex-col gap-7 sm:w-2/5 lg:w-1/4 w-full' key={index}>
-              <img src={items.urlToImage} alt="" className='w-full h-full' />
+              <img src={items.image} alt="" className='w-full h-full' />
               <h1 className='font-bold text-xl'>{items.title}</h1>
               <p className='text-black/50 line-clamp-2 text-ellipsis'>{items.content}</p>
               <a href={items.url} target="_blank" rel="noopener noreferrer"className='bg-red text-white p-4 w-1/2 text-center'>Read More</a>
